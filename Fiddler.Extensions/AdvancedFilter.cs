23bf3a262c21bf8bbe1ffef10870055d0897551d
Kashif Khan <kashif.k.khan@ericsson.com>
2018-07-30 01:32:39 +0530

    Initial commit

---
+++ Fiddler.Extensions/AdvancedFilter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows.Forms;
+using Fiddler;
+using Fiddler.Extensions.UI.Tabs;
+
+[assembly: RequiredVersion("2.3.5.0")]
+namespace Fiddler.Extensions
+{
+    public class AdvancedFilter : IAutoTamper, IDisposable
+    {
+        private AdvancedFilterTab advancedFilterTab;
+        public void AutoTamperRequestAfter(Session oSession)
+        {
+        }
+
+        public void AutoTamperRequestBefore(Session oSession)
+        {
+        }
+
+        public void AutoTamperResponseAfter(Session oSession)
+        {
+        }
+
+        public void AutoTamperResponseBefore(Session oSession)
+        {
+        }
+
+        public void Dispose()
+        {
+            advancedFilterTab.Dispose();
+        }
+
+        public void OnBeforeReturningError(Session oSession)
+        {
+        }
+
+        public void OnBeforeUnload()
+        {
+        }
+
+        public void OnLoad()
+        {
+            advancedFilterTab = new AdvancedFilterTab();
+            FiddlerApplication.UI.tabsViews.TabPages.Add(advancedFilterTab);
+        }
+    }
+}
---
+++ Fiddler.Extensions/UI/Tabs/AdvancedFilterTab.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fiddler.Extensions.UI.Tabs
+{
+    internal class AdvancedFilterTab : TabPage
+    {
+        public AdvancedFilterTab() : this("Advanced Filter")
+        {
+            ImageIndex = (int)SessionIcons.FilterAll;
+        }
+
+        public AdvancedFilterTab(string text) : base(text)
+        {
+        }
+    }
+}
